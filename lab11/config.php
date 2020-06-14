30acdca38c24b8e34fafe39becb7f7b92e4f6f1f
Panpanmojue <61504198+Panpanmojue@users.noreply.github.com>
2020-06-14 23:11:17 +0800

    upload lab11

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'Login');
+define('DBUSER', 'Panpanmojue');
+define('DBPASS', '2002524.xmj');
+define('DBCONNSTRING','mysql:host=localhost;dbname=Login');
+?>
